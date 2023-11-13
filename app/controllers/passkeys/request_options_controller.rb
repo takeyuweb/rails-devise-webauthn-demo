7f34ea1f2232721c3a47f21e9ce43b87cb368276
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2023-11-13 03:46:45 +0000

    オートフィルに対応する

--- app/controllers/passkeys/request_options_controller.rb
+++ app/controllers/passkeys/request_options_controller.rb
@@ -1,12 +1,12 @@
 class Passkeys::RequestOptionsController < ApplicationController
   def create
     user_params = params.require(:user).permit(:email)
-    user = User.find_by!(email: user_params[:email])
+    user = User.find_by(email: user_params[:email])
 
     # https://www.w3.org/TR/webauthn/#dictionary-assertion-options
     request_options = WebAuthn::Credential.options_for_get(
       user_verification: "required",  # ユーザー認証を要求する
-      allow: user.passkeys&.pluck(:external_id),  # メールアドレスからユーザーが特定し、登録済みのパスキーで絞り込む
+      allow: user ? user.passkeys.pluck(:external_id) : nil,  # メールアドレスからユーザーが特定できれば、登録済みのパスキーで絞り込む
     )
 
     session[:current_webauthn_authentication_challenge] = request_options.challenge
--- app/javascript/controllers/authorize_passkey_controller.js
+++ app/javascript/controllers/authorize_passkey_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 import {
+  supported,
   get,
   parseRequestOptionsFromJSON
 } from "@github/webauthn-json/browser-ponyfill";
@@ -10,12 +11,33 @@ export default class extends Controller {
     requestOptionsUrl: String
   };
 
+  async formTargetConnected(element) {
+    // Check if the browser supports WebAuthn.
+    if (supported()) {
+      if (PublicKeyCredential.isConditionalMediationAvailable) {
+        // Check if the browser supports conditional mediation.
+        // https://developer.mozilla.org/en-US/docs/Web/API/PublicKeyCredential/isConditionalMediationAvailable
+        const isCMA = await PublicKeyCredential.isConditionalMediationAvailable();
+        if (isCMA) {
+          console.log("CMA is available");
+          this.getChallengeAndSubmitCredential(isCMA);
+        } else {
+          console.log("CMA is not available");
+        }
+      } else {
+        console.log("CMA is not supported");
+      }
+    } else {
+      console.log("WebAuthn is not supported");
+    }
+  }
+
   signInWithPasskey(event) {
     event.preventDefault();
     this.getChallengeAndSubmitCredential();
   }
 
-  getChallengeAndSubmitCredential() {
+  getChallengeAndSubmitCredential(isCMA) {
     const data = new FormData(this.formTarget);
     data.delete('_method');
 
@@ -32,6 +54,11 @@ export default class extends Controller {
         console.log(requestOption);
         // ここに成功時の処理を書く
         const credentialRequestOptions = parseRequestOptionsFromJSON({ publicKey: requestOption });
+        if (isCMA) {
+          // Conditional Mediation の場合は "conditional" を指定する
+          // https://w3c.github.io/webappsec-credential-management/#mediation-requirements
+          credentialRequestOptions['mediation'] = 'conditional';
+        }
         const credentialRequestResponse = await get(credentialRequestOptions);
         this.credentialFieldTarget.value = JSON.stringify(credentialRequestResponse);
         this.formTarget.submit();
